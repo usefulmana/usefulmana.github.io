791641e629dc08847b9adcf44b4a74fc15705058
Alex Nguyen <alex.nguyen.3141@gmail.com>
2019-10-23 09:50:16 +0700

    Remove hardcoded github login credentials

--- scripts/getRepos.js
+++ scripts/getRepos.js
@@ -1,39 +1,40 @@
-
 $(document).ready(function() {
-  $.ajax({
-    type: 'GET',
-    url:
-      'https://api.github.com/user/repos?type=public&sort=updated&direction=desc',
-    dataType: 'json',
-    headers: {
-      Authorization: 'Basic ' + btoa('usefulmana' + ':' + 'GAtech321')
-    },
-    success: function(repos) {
-      $('#repo_table').DataTable({
-        data: repos.slice(0, 10),
-        order: [[4, 'desc']],
-        info: false,
-        paging: false,
-        searching: false,
-        responsive: true,
-        columns: [
-          { data: 'name' },
-          { data: 'stargazers_count' },
-          { data: 'watchers_count' },
-          {
-            data: 'html_url',
-            render: function(data, type, row, meta) {
-              if (type === 'display') {
-                data =
-                  '<a href="' + data + '" target="_blank">' + "Link" + '</a>';
-              }
+  $.getJSON('../credentials/credentials.json', function(credentials) {
+    $.ajax({
+      type: 'GET',
+      url:
+        'https://api.github.com/user/repos?type=public&sort=updated&direction=desc',
+      dataType: 'json',
+      headers: {
+        Authorization: 'Basic ' + btoa(credentials.username + ':' + credentials.password)
+      },
+      success: function(repos) {
+        $('#repo_table').DataTable({
+          data: repos.slice(0, 10),
+          order: [[4, 'desc']],
+          info: false,
+          paging: false,
+          searching: false,
+          responsive: true,
+          columns: [
+            { data: 'name' },
+            { data: 'stargazers_count' },
+            { data: 'watchers_count' },
+            {
+              data: 'html_url',
+              render: function(data, type, row, meta) {
+                if (type === 'display') {
+                  data =
+                    '<a href="' + data + '" target="_blank">' + 'Link' + '</a>';
+                }
 
-              return data;
-            }
-          },
-          { data: 'updated_at' }
-        ]
-      });
-    }
+                return data;
+              }
+            },
+            { data: 'updated_at' }
+          ]
+        });
+      }
+    });
   });
 });
