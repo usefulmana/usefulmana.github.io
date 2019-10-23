38a2bea53c44c84fbfc479b0942045aec45ac0e6
Alex Nguyen <alex.nguyen.3141@gmail.com>
2019-10-23 09:21:45 +0700

    Finish repo table

--- scripts/getRepos.js
+++ scripts/getRepos.js
@@ -1,12 +1,37 @@
+// const credentials = require('../credentials/credentials.json')
+// console.log(credentials)
 $(document).ready(function() {
   $.ajax({
     type: 'GET',
-    url: 'https://jsonplaceholder.typicode.com/users',
+    url:
+      'https://api.github.com/user/repos?type=public&sort=updated&direction=desc',
+    dataType: 'json',
+    headers: {
+      Authorization: 'Basic ' + btoa('usefulmana' + ':' + 'GAtech321')
+    },
     success: function(repos) {
-      $.each(repos, function(i, repo) {
-        $('#repos').append('<li>' + '<a>' + repo.name + '</a>' + '</li>');
-        $('#repos a').attr('href', '#home');
-        $('#repos a').addClass('scroll-item');
+      $('#repo_table').DataTable({
+        data: repos.slice(0, 10),
+        order: [[4, 'desc']],
+        info: false,
+        paging: false,
+        searching: false,
+        columns: [
+          { data: 'name' },
+          { data: 'stargazers_count' },
+          { data: 'watchers_count' },
+          {
+            data: 'html_url',
+            render: function(data, type, row, meta) {
+              if (type === 'display') {
+                data = '<a href="' + data + '" target="_blank">' + data + '</a>';
+              }
+
+              return data;
+            }
+          },
+          { data: 'updated_at' }
+        ]
       });
     }
   });
